227117b11dc65c48eabcdd374f3a00fecdc48522
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-11 18:38:46 -0500

    gradient added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__items">
-        <button className="navbar__items-donate">Donate</button>
+      <div className="navbar__container">
+        <div className="navbar__items">
+          <button className="navbar__items-donate">Donate</button>
+        </div>
       </div>
     </nav>
   );
