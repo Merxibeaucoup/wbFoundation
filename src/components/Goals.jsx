bd856ce92756a29a211129206ba1cd55a70b0314
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-11 22:47:03 -0500

    ggggoals

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 
 const Goals = () => {
   const [state] = useState({
-    heading:
-      "3 in 10 people in Ghana lack access to clean water and quality education. We're on a MISSION to change that. Here is how: ",
+    heading: "We're on a MISSION to change other people's lives. Here is how: ",
     paragraph:
       "voluptas alias voluptatum accusamus similique neque. Ab, necessitatibus?consectetur adipisicing elit. Vel est eligendi at sit autem, necessitatibus debitis eos repudiandae nostrum quia soluta harum voluptas alias voluptatum accusamus similique neque. Ab, necessitatibus?",
   });
--- src/components/GoalsList.jsx
+++ src/components/GoalsList.jsx
@@ -2,83 +2,19 @@ import React from "react";
 
 const GoalsList = () => {
   return (
-    <div className="goals__list">
+    <div className="goals">
       <div className="container">
-        {/* first */}
-        <div className="row mr-minus-15">
-          <div className="col-6 p-15">
-            <div className="goals__list__texts">
-              <p className="goals__list__texts-number">01</p>
-              <h3 className="goals__list__texts-heading">
-                Provide clean and safe drinking water
-              </h3>
-              <p className="goals__list__texts-paragraph">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Distinctio, deleniti saepe voluptates labore eligendi nesciunt
-                debitis voluptatem rem sunt vel iure perferendis numquam modi
-                excepturi quaerat consequuntur pariatur voluptas provident sequi
-                odit illo? Magni laudantium minus fugit voluptates cumque nam
-              </p>
-            </div>
+        <div className="goals__boxes">
+          <div className="goals__boxes-box1">box1</div>
+          <div
+            className="goals__boxes-box2"
+            style={{ backgroundImage: "url(/public/assets/support/hope.jpg)" }}
+          >
+            box2
           </div>
-          <div className="col-6 p-15">
-            <div className="goals__list__img">
-              <img src="/assets/goals/g1.jpg" alt="goal1" />
-            </div>
-          </div>
+          <div className="goals__boxes-box3">box3</div>
+          <div className="goals__boxes-box4">box4</div>
         </div>
-        {/* second */}
-        <div className="row mr-minus-15">
-          <div className="col-6 p-15">
-            <div className="goals__list__img">
-              <img src="/assets/goals/g3.jpg" alt="goal1" />
-            </div>
-          </div>
-          <div className="col-6 pl-15">
-            <div className="goals__list__texts">
-              <p className="goals__list__texts-number">02</p>
-              <h3 className="goals__list__texts-heading">
-                Provide Quality education
-              </h3>
-              <p className="goals__list__texts-paragraph">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Distinctio, deleniti saepe voluptates labore eligendi nesciunt
-                debitis voluptatem rem sunt vel iure perferendis numquam modi
-                excepturi quaerat consequuntur pariatur voluptas provident sequi
-                odit illo? Magni laudantium minus fugit voluptates cumque nam
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* third */}
-        {/* <div className="row mr-minus-15">
-          <div className="col-6 p-15">
-            <div className="goals__list__texts">
-              <p className="goals__list__texts-number">03</p>
-              <h3 className="goals__list__texts-heading">
-                Provide warm good food
-              </h3>
-              <p className="goals__list__texts-paragraph">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Distinctio, deleniti saepe voluptates labore eligendi nesciunt
-                debitis voluptatem rem sunt vel iure perferendis numquam modi
-                excepturi quaerat consequuntur pariatur voluptas provident sequi
-                odit illo? Magni laudantium minus fugit voluptates cumque nam
-                consequuntur pariatur voluptas provident sequi odit illo? Magni
-                laudantium minus fugit voluptates cumque nam
-              </p>
-            </div>
-          </div>
-          <div className="col-6 p-15">
-            <div className="goals__list__img">
-              <img
-                style={{ filter: "grayscale(60%)" }}
-                src="/assets/goals/w4.jpg"
-                alt="goal1"
-              />
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
