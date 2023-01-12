a6ae6e13c8b78606619086e72e6450545a63c554
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-11 20:46:25 -0500

    arr2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   const [state] = useState({
     image: "assets/header/he1.jpg",
     logo: "assets/logo/wb-5.png",
-    title: "",
+    title: "Givers Never lack",
+    para: "ahsiaaslknlqw wkdnkjew eknfewj qkneqwj kewqnfjewq qwknwqdqwenfljweq sncwq jn dfn ",
+    swirly: "assets/header/sw1.svg",
   });
 
   return (
@@ -17,6 +19,19 @@ const Header = () => {
 
         <div className="header__img">
           <LazyLoadImage src={state.image} alt="image" />
+        </div>
+        <div className="header__words">
+          <div className="container">
+            <div className="header__words__text">
+              <h2 className="header__words__text-title">{state.title}</h2>
+              <p className="header__words__text-paragraph">
+                &ldquo;{state.para} &rdquo;
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="header__arrow">
+          <img src={state.swirly} alt="" />
         </div>
       </div>
     </div>
