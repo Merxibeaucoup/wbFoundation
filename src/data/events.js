ca50ce943425457d3b4701fe382cf8c812dbaa6f
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-12 23:26:34 -0500

    data events

---
+++ src/data/events.js
@@ -0,0 +1,28 @@
+const Events = [
+  {
+    id: 1,
+    image: "/assets/support/coins.jpg",
+    title: "Provisions , Meal Packaging & Delivery",
+    desc: "Your generous support drives our mission and enables us to make an impact in our community. You are vital to the work that we do.",
+    date: "December",
+    location: "Nsawam",
+  },
+  {
+    id: 2,
+    image: "/assets/support/dove.jpg",
+    title: "Back to School Drive",
+    desc: "Becoming a WBFoundation Team sponsor is a smart and strategic way to build brand awareness and strengthen your consumer loyalty",
+    date: "January",
+    location: "Accra",
+  },
+  {
+    id: 3,
+    image: "/assets/support/soccer.jpg",
+    title: "Community sports and games",
+    desc: "Building solid relationships go a long way. One way of doing that is by doing activities we enjoy together. ",
+    date: "January",
+    location: "Accra",
+  },
+];
+
+export default Events;
