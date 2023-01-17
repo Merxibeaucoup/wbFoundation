efe20ba53d34cc12998895da07984d2ac4ca8b43
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-16 19:55:23 -0500

    done

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const EventList = ({ item }) => {
   return (
     <div>
       <div
         className="events__box"
-        style={{
-          backgroundImage: `url(${item.image}`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          width: "380px",
-        }}
+        // style={{
+        //   backgroundImage: `url(${item.image}`,
+        //   backgroundPosition: "center",
+        //   backgroundSize: "cover",
+        //   backgroundRepeat: "no-repeat",
+        //   width: "380px",
+        // }}
       >
-        <h3 className="events__box-title">{item.title}</h3>
-        <p className="events__box-desc">{item.desc}</p>
-        <p className="events__box-date-loc">
-          {item.date} | {item.location}
-        </p>
-        <button className="btn btn--form">Donate</button>
+        <div className="events__box-img">
+          <LazyLoadImage src={item.image} alt={item.image} />
+        </div>
+        <div className="events__box-contents">
+          <h3 className="events__box-title">{item.title}</h3>
+          <p className="events__box-desc">{item.desc}</p>
+          <p className="events__box-date-loc">
+            {item.date} | {item.location}
+          </p>
+          <button className="btn btn--form">{item.action}</button>
+        </div>
       </div>
     </div>
   );
--- src/data/events.js
+++ src/data/events.js
@@ -1,19 +1,21 @@
 export const eventsData = [
   {
     id: 1,
-    image: "/assets/support/coins.jpg",
+    image: "/assets/goals/w4.jpg",
     title: "Provisions & Meal Packaging ",
     desc: "Your generous support drives our mission and enables us to make an impact in our community. You are vital to the work that we do.",
     date: "December",
     location: "Nsawam",
+    action: "Enquire",
   },
   {
     id: 2,
-    image: "/assets/support/dove.jpg",
-    title: "Back to School Drive & give away",
+    image: "/assets/support/ghs2.jpeg",
+    title: "Back to School Drive ",
     desc: "Becoming a WBFoundation Team sponsor is a strategic way to build brand awareness and strengthen consumer loyalty.",
     date: "January",
     location: "Accra",
+    action: "Enquire",
   },
   {
     id: 3,
@@ -22,5 +24,6 @@ export const eventsData = [
     desc: "Building solid relationships go a long way. One way of doing that is by doing activities we enjoy together. playing Friendly competitive games. ",
     date: "January",
     location: "Accra",
+    action: "Enquire",
   },
 ];
