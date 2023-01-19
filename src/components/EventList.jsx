1fadad37c952ace258eeeb0f2e3a09ecead2bab1
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-18 20:33:43 -0500

    modal style started

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-responsive-modal/styles.css";
+import Modal from "react-responsive-modal";
 
 const EventList = ({ item }) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  const bg = {
+    overlay: {
+      background: "rgba(23, 25, 26, 0.544)",
+    },
+    modal: {
+      background: "#b2dbbf",
+      width: "500px",
+      height: "100px",
+    },
+  };
   return (
     <div>
-      <div
-        className="events__box"
-        // style={{
-        //   backgroundImage: `url(${item.image}`,
-        //   backgroundPosition: "center",
-        //   backgroundSize: "cover",
-        //   backgroundRepeat: "no-repeat",
-        //   width: "380px",
-        // }}
-      >
+      <div className="events__box">
         <div className="events__box-img">
           <LazyLoadImage src={item.image} alt={item.image} />
         </div>
@@ -23,7 +31,18 @@ const EventList = ({ item }) => {
           <p className="events__box-date-loc">
             {item.date} | {item.location}
           </p>
-          <button className="btn btn--form">{item.action}</button>
+          <button onClick={onOpenModal} className="btn btn--form">
+            {item.action}
+          </button>
+          <Modal open={open} onClose={onCloseModal} center styles={bg}>
+            <form action="POST" style={{ display: "flex", gap: "1rem" }}>
+              <input type="text" placeholder="fullname" />
+
+              <input type="email" />
+
+              <button type="submit">Submit</button>
+            </form>
+          </Modal>
         </div>
       </div>
     </div>
