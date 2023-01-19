01a9676c0c6e798558c1fa1ccf7cb33e404745ec
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-19 17:43:23 -0500

    donate page done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Link } from "react-router-dom";
 
 import StripeCheckout from "react-stripe-checkout";
-import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Checkout() {
+  const [state] = useState({
+    logo: "assets/logo/wb-5.png",
+    image: "assets/donate/heart1.png",
+    support:
+      "Please Show your support with a donation of your choice. This will only take a minute.",
+  });
   var [number, setNumber] = useState("");
 
   const numberHandle = (event) => {
@@ -34,25 +42,45 @@ function Checkout() {
   };
   return (
     <>
-      <Header />
       <div className="container">
-        <div>
-          <label>Enter amount to donate</label>
-          <input
-            onChange={numberHandle}
-            type="number"
-            placeholder="Enter Number"
-            value={number}
-          />
+        <div
+          className="donate"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, #eb240a9d , #eb240a2e 48%), url(${state.image})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <Link to="/">
+            <div className="header__logo">
+              <LazyLoadImage src={state.logo} alt="logo" />
+            </div>
+          </Link>
+          <div>
+            <h3 className="donate__heading">{state.support}</h3>
+          </div>
+          <div className="donate__form">
+            <input
+              onChange={numberHandle}
+              type="number"
+              min="1"
+              // onkeypress="return event.charCode >= 48"
+              pattern="^[0-9]"
+              placeholder="Enter Number"
+              value={number}
+            />
 
-          <StripeCheckout
-            stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            token={makeRequest}
-            name="WB foundation"
-            amount={number * 100}
-          >
-            <button className="btn-large ">Donate {number} $</button>
-          </StripeCheckout>
+            <StripeCheckout
+              stripeKey={process.env.REACT_APP_STRIPE_KEY}
+              token={makeRequest}
+              name="WB foundation"
+              amount={number * 100}
+            >
+              <button className="btn ">Donate {number} $</button>
+            </StripeCheckout>
+          </div>
+          <h4 className="donate__thanks">Thank you!</h4>
         </div>
       </div>
     </>
