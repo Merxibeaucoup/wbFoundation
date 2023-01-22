76ae591b1cea3f80c7d1a6794ca507691ffce4ea
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-22 18:43:05 -0500

    uuid fixed

--- stripeBackend/index.js
+++ stripeBackend/index.js
@@ -1,10 +1,10 @@
 const cors = require("cors");
 const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_KEY);
-const {v4: uuidv4}= require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -17,50 +17,41 @@ app.get("/", (req, res) => {
   res.send("IT WORKS ");
 });
 
-
 app.post("/checkout", async (req, res) => {
   const { number, token } = req.body;
   console.log("AMOUNT ", number);
   const idempotencyKey = uuidv4();
 
   const lineItems = [];
-  
 
-  
-
-  
- await stripe.checkout.sessions.create({
-  line_items: [
-    {name:'merci',currency: 'usd', quantity: 1, amount: number*100,},
-  ],
-    success_url: 'http://localhost:3000/success',
-    cancel_url: 'https://localhost:3000/cancel',
-    payment_method_types: ['card'],
-    mode:'payment',
-  
-  })
+  await stripe.checkout.sessions.create({
+    line_items: [
+      { name: "merci", currency: "usd", quantity: 1, amount: number * 100 },
+    ],
+    success_url: "http://localhost:3000/success",
+    cancel_url: "https://localhost:3000/cancel",
+    payment_method_types: ["card"],
+    mode: "payment",
+  });
 
   return stripe.customers
     .create({
       email: token.email,
-      source: token.id
+      source: token.id,
     })
-    .then(customer => {
+    .then((customer) => {
       stripe.charges.create(
         {
           amount: number * 100,
           currency: "usd",
           customer: customer.id,
           receipt_email: token.email,
-          
         },
-        { idempotencyKey: idempotencyKey}
+        { idempotencyKey: idempotencyKey }
       );
     })
-    .then(result => res.status(200).json(result))
-    .catch(err => console.log(err));
-    
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
 });
 
 app.listen(8282, () => console.log("LISTENING AT PORT 8282"));
-
